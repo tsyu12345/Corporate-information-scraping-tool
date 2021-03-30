054f5c29790cf39f230a215a524a9a355ea27941
syu-subPC <syukunt@icloud.com>
2021-03-30 18:42:13 +0900

    urlの自動取得は完成、未決：手動でセレクタを取得し、その要素値をExcelへ入力、SSLの有無の自動判定、閉鎖されているサイトの除外

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
-import pandas as pd
+
 import requests as rq
 import openpyxl
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
+from requests.exceptions import SSLError 
 
+name = "【高林様】抽出依頼リスト - コピー.xlsx"
+book = openpyxl.load_workbook(name)
+sheet = book.worksheets[0]
+
+#list of urls
+urls = []
+for url in sheet["C"]:
+    if url.value != None or url.value != "ドメイン名":
+        urls.append(url.value)
+        print(url.value)
+
+print(urls[2])
+
+#サイトが応答するか（存在するか確認）
+erorr_site = []
+for i in range(2, len(urls)):
+    respons = rq.get("http://" + urls[i])
+    site_status = respons.status_code
+    print(str(i) + ". " + urls[i] + " : ", end="")
+    if site_status != 200:
+        print("site Erorr!!, please check this page")
+        erorr_site.append(urls[i])
+        sheet.cell(row=i+1, column=1, value="x")
+book.save(name)
+
+#スクレイピング
+def scrap(html):
+    soup = BeautifulSoup()
+
+    
+
+
+
+ 
