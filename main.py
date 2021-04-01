b40e993a7f3bc067814a22d8c9696d9c16612d59
syu <syukunt@icloud.com>
2021-04-01 16:59:40 +0900

    3rd commit : 会社名を手動入力し、その他の情報はGoogleMapからスクレイピングする手法で計画中

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from selenium import webdriver
 from openpyxl import styles as pxstyle
 import openpyxl as px 
 from bs4 import BeautifulSoup as bs 
-import ssl, socket
+import time
+
+from selenium.webdriver.chrome.webdriver import WebDriver
+
 
 #sheetの読み込み
 file_name = "./【高林様】抽出依頼リスト - コピー.xlsx"
@@ -12,7 +15,8 @@ book = px.load_workbook(file_name)
 sheet = book.worksheets[0]
 
 #サイトの存在確認
-fill = pxstyle.PatternFill(patternType='solid', fgColor='FF0000', bgColor='FF0000')#応答がないサイトには赤色で塗る
+fill_red = pxstyle.PatternFill(patternType='solid', fgColor='FF0000', bgColor='FF0000')#応答がないサイトには赤色で塗る
+fill_whi = pxstyle.PatternFill(patternType='solid', fgColor='ffffff', bgColor='ffffff')
 for i in range(3, sheet.max_row):
     index = "C"+str(i)
     url = sheet[index].value
@@ -21,11 +25,13 @@ for i in range(3, sheet.max_row):
         respons = rq.get("http://www." + url)
         if respons.status_code != 200:#閲覧できないとき
             print(respons.status_code)
-            sheet[index].fill = fill
+            sheet[index].fill = fill_red
+            sheet["D" + str(i)].value = "取得不可"
         else:#通常通り
             page_url = respons.url
             print(respons.status_code)
             print(page_url)
+            sheet[index].fill = fill_whi
             #SSLかどうか（httpsで始まっているかどうか）
             if page_url.startswith("https://"):
                 print("SSL 有")
@@ -35,13 +41,9 @@ for i in range(3, sheet.max_row):
             
     except:#そもそもサイトが存在しない？？
         print(respons.status_code)
-        sheet[index].fill = fill
+        sheet[index].fill = fill_red
+        sheet["D"+str(i)].value = "不明なエラー"
         pass
 book.save(file_name)
-
-
-
-        
-
 
 
